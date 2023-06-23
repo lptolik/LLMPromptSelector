e07b0b66ff01c872c09ccfbdf1f4ebc545a78899
Aleksandr Sorokin <asoroki2@ed.ac.uk>
2023-06-23 11:32:44 +0100

    custom-agent pretty much done

--- custom-chat-agent-prototype.py
+++ custom-chat-agent-prototype.py
@@ -3,13 +3,14 @@ OPENAI_API_KEY = "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"
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
 from langchain.document_loaders import CSVLoader
+from langchain.document_loaders import PyPDFium2Loader
+from langchain.document_loaders import UnstructuredPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.llms import OpenAI
 from langchain.chains.question_answering import load_qa_chain
 
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Weaviate
-from langchain.chains import ConversationalRetrievalChain
 from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
 WEAVIATE_URL = "https://first-test-cluster-dw7v1rzb.weaviate.network"
@@ -20,6 +21,7 @@ from langchain.prompts import BaseChatPromptTemplate
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 
+# Global model used for both the agent and the prompt:
 llm = ChatOpenAI(
         openai_api_key=OPENAI_API_KEY,
         model_name='gpt-3.5-turbo',
@@ -27,6 +29,7 @@ llm = ChatOpenAI(
         verbose=True
     )
 
+# Defining the actual answering system:
 prompt =  PromptTemplate(
             input_variables=["user_input", "context"],
             template="""Act as a friendly wise old man, giving advice learned from a long life. Use a friendly and familiar, yet authoritative and expert tone and provide anecdotes or metaphors with any advice you give. You like frequently making philosophical metaphors. You are quite self-reflective and somewhat of a philosopher. You have made mistakes in your past and so you have many regrets, you want to help others avoid doing the same. You seek to pass down everything youve learned to others so your responses often refer to your own experiences. Your objective is to give advice and guidance based on a user input. Structure your response in paragraphs.
@@ -38,21 +41,6 @@ prompt =  PromptTemplate(
 
 selected_prompt_chain = LLMChain(llm=llm, prompt=prompt)
 
-from pydantic import BaseModel, Field
-
-# class WOMInput(BaseModel):
-#     user_input: str = Field()
-#     context: str = Field()
-
-# tools = [Tool(
-#             name="Wise Old Man",
-#             func=selected_prompt_chain.run,
-#             description=(
-#                 'Useful for constructing the Final Answer. The action input is the exact user query and you can provide context. Always use this tool last.'
-#             ),
-#             #args_schema=WOMInput
-#         )]
-
 main_tool = Tool(
     name="Wise Old Man",
     func=selected_prompt_chain.run,
@@ -62,20 +50,33 @@ main_tool = Tool(
 )
 
 tools_list = []
+chat_history = []
+
+def list_to_string():
+    output = ""
+    for i in chat_history:
+        output += i
+    return output
 
 def split_data(file_name):
-    with open(file_name) as f:
-        csv = f.read()
-
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size = 1000,
-        chunk_overlap = 20,
-        length_function = len,
-        add_start_index = True
-    )
+            chunk_size = 1000,
+            chunk_overlap = 20,
+            length_function = len,
+            add_start_index = True
+        )
+    
+    if file_name[-4:] == ".csv" or file_name[-4:] == ".txt":
+        with open(file_name) as f:
+            csv = f.read()
 
-    split_csv = text_splitter.create_documents([csv])
-    return split_csv
+        documents = text_splitter.create_documents([csv])
+        
+    if file_name[-4:] == ".pdf":
+        loader = UnstructuredPDFLoader(file_name)
+        documents = loader.load()   # not sure if I should split this for larger pdfs
+        
+    return documents
 
 def init_qa(db):
 
@@ -132,6 +133,8 @@ class CustomPromptTemplate(BaseChatPromptTemplate):
         kwargs["tools"] = "\n".join([f"{tool.name}: {tool.description}" for tool in self.tools])
         # Create a list of tool names for the tools provided
         kwargs["tool_names"] = ", ".join([tool.name for tool in self.tools])
+        # # Format chat historyL
+        # kwargs["chat_history"] = list_to_string()
         formatted = self.template.format(**kwargs)
         return [HumanMessage(content=formatted)]
     
@@ -160,32 +163,6 @@ class CustomOutputParser(AgentOutputParser):
         return AgentAction(tool=action, tool_input=action_input.strip(" ").strip('"'), log=llm_output)
     
 def update_template():
-    # Set up the base template
-    # template = """You must respond to the input to the best of your ability. NEVER REPHRASE THE USER QUERY. You have access to the following tools to get relevant information/context to the question:
-
-    # {tools}
-    
-    # Additionally, if you dont know what to do, respond using {main_tool}
-
-    # Use the following format:
-
-    # Query: the input you must respond to
-    # Thought: you should always think about what to do
-    # Action: the action to take, should be one of [{tool_names}] if you require additonal context, or {main_tool_name} if you are ready to respond.
-    # Action Input: the input to the action
-    # Observation: the result of the action
-    # ... (this Thought/Action/Action Input/Observation can repeat 2 times)
-    # Thought: I now know the relevant information
-    # Final Answer: the final answer to the original input query, generated using {main_tool}
-
-    # These were previous tasks you completed:
-
-
-
-    # Begin!
-
-    # Query: {input}
-    # {agent_scratchpad}"""
     
     template = """You must respond to the input to the best of your ability.
     You have access to the following tools to get relevant information/context to the question: 
@@ -197,13 +174,16 @@ def update_template():
     Query: the input you must respond to
     Thought: you should always think about what to do
     Action: the action to take, should be one of [{tool_names}]
-    Action Input: the input to the action
+    Action Input: the input to the action, should be as close to the original user query as possible
     Observation: the result of the action
     ... (this Thought/Action/Action Input/Observation can repeat N times)
     Thought: I now know the final answer
     Final Answer: the final answer to the original input question
 
     IF YOU DONT USE ANY TOOL YOUR OUTPUT SHOULD BE EMPTY!! YOUR ONLY OUTPUT SHOULD BE: "N/A"
+
+    Chat history:
+    {chat_history}
 
     Begin!
     
@@ -214,14 +194,15 @@ def update_template():
     prompt = CustomPromptTemplate(
         template=template,
         tools=tools_list,
-        input_variables=["input", "intermediate_steps"]
+        input_variables=["input", "intermediate_steps", "chat_history"]
     )
     
     return prompt
 
 def set_up():
     load_new_data("UniFAQdata.csv", "University FAQ Knowledge Base", 'useful for answering frequently asked questions about University.')
-    load_new_data("sample.csv", "Companies Knowledge Base", 'useful for answering questions regarding companies information')
+    #load_new_data("sample.csv", "Companies Knowledge Base", 'useful for answering questions regarding companies information')
+    load_new_data("2023 - Student Induction.pdf", "Student Induction Knowledge Base", "useful for answering questions regarding new student intern questions")
     
     prompt = update_template()
     
@@ -246,8 +227,9 @@ agent_exec = set_up()
 user_input = input()
 
 while user_input != "":
-    #agent_exec.run({"input": user_input, "main_tool": main_tool})
-    response = agent_exec.run(user_input)
+    response = agent_exec.run({"input" : user_input, "chat_history": list_to_string()})#user_input, chat_history=list_to_string())
+    chat_history.append(f"User query: {user_input}\n")
+    chat_history.append(f"Agent response: {response}\n")
     print(f"This is the agent response: {response}")
     if response == "N/A":
         response=""
